847ba0328aa8488756ac2e567fc76b059d4e208b
helensui <helensqj@hotmail.com>
2020-01-12 21:20:12 -0800

    upload to git

---
+++ precache-manifest.0f080c684ce363ee6825b2686c868d2b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a6c391c54f3b3eeef99fe98acb95ac",
+    "url": "/brand-insights/index.html"
+  },
+  {
+    "revision": "abc4f636906b80d3bccb",
+    "url": "/brand-insights/static/js/2.04a506a1.chunk.js"
+  },
+  {
+    "revision": "3000e0442341a9e4c2b1",
+    "url": "/brand-insights/static/js/main.0b00c2d8.chunk.js"
+  },
+  {
+    "revision": "ec2125e501b68eb08da8",
+    "url": "/brand-insights/static/js/runtime~main.6164cb62.js"
+  }
+]);
